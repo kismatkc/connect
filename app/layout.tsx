ae997dd88420fb056a4ae933866d8ad77e892f7e
kismatkc <52825603+kismatkc@users.noreply.github.com>
2024-10-21 12:21:52 +0000

    auto

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className={`${interFont.className} antialiased`}>
         <ThemeProvider
           attribute="class"
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ThemeToggleButton from "./theme-toggler-button";
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 
 const Header = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
+  useEffect(()=>{
+    setMounted(true)
+  })
 
+  if (!mounted) return null;
   return (
     <header className="flex justify-between p-2">
       <div className="flex">
@@ -35,10 +40,27 @@ const Header = () => {
           <Image
             src="/header/facebook-messenger.svg"
             alt="messenger icon"
-            width={24}
-            height={24}
+            width={32}
+            height={32}
             className="h-8 w-8"
+            priority
           />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-full bg-[rgb(228,230,235)] dark:bg-[rgb(255,255,255,.1)] pr-1 pb-1"
+        >
+          <Image
+            src="/header/facebook-messenger.svg"
+            alt="messenger icon"
+            width={32}
+            height={32}
+            className="h-8 w-8"
+            priority
+          />
+       
           <span className="sr-only">Toggle theme</span>
         </Button>
       </div>
